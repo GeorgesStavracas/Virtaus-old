dd6fa29abab09c01d5f19171ef7d982a5b31686a
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2014-03-25 15:45:50 -0300

    core: initial Settings method implementation

--- core/settings.cpp
+++ core/settings.cpp
@@ -1,6 +1,36 @@
 #include "settings.h"
 
-Settings::Settings(QObject *parent) :
-    QObject(parent)
+using namespace Virtaus;
+
+Settings* Virtaus::Settings::instance = NULL;
+
+Settings::Settings() : QObject()
 {
+    QDir* dir = new QDir;
+    this->config_file = new QString(dir->currentPath()+"/config.ini");
+    qDebug() << (*this->config_file);
+    this->settings = new QSettings(*this->config_file);
+}
+
+void
+Settings::setFile(QString &file)
+{
+    this->config_file->clear();
+    this->config_file->append(file);
+
+    delete this->settings;
+    this->settings = new QSettings(*this->config_file);
+
+    qDebug() << (*this->config_file);
+}
+
+Settings*
+Settings::getInstance()
+{
+    if (!instance) {
+        instance = new Settings;
+        qDebug() << "Instance created";
+    }
+
+    return instance;
 }
--- core/settings.h
+++ core/settings.h
@@ -1,13 +1,33 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <QObject>
+#include <QtCore>
+#include <QSettings>
 
-class Settings : public QObject
+namespace Virtaus {
+    class Settings;
+}
+
+class VirtausApplication;
+
+class Virtaus::Settings : public QObject
 {
+
     Q_OBJECT
+
 public:
-    explicit Settings(QObject *parent = 0);
+    static Virtaus::Settings* getInstance();
+
+    void setFile(QString& file);
+
+    template<typename T> T setting(QString& field);
+
+private:
+    static Settings* instance;
+    QString* config_file;
+    QSettings* settings;
+
+    Settings();
 
 signals:
 
